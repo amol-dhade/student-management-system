1edec44faab4fdb5cc38be091bf505efcd69e9d5
Amol Dhade <amoldhade007@gmail.com>
2023-04-07 17:04:21 +0530

    last commit

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('student/form/', views.stu_view, name='student_form_url'),
     path('student/list/', views.stu_list, name='student_list_url'),
+    path('student/update/<int:pk>/', views.update_student, name='student_update_url'),
+    path('student/delete/<int:pk>/', views.delete_student, name='student_delete_url')
 ]
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student 
 from .forms import StudentForm
 
@@ -10,6 +10,7 @@ def stu_view(request):
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('student_list_url')
     context = {'form':form}
     return render(request, template_name, context)
 
@@ -18,5 +19,29 @@ def stu_list(request):
     template_name = 'app1/student_list.html' 
     context = {'data':data}
     return render(request, template_name, context)
+
+def update_student(request, pk):
+    obj = Student.objects.get(pk=pk)
+    form = StudentForm(instance=obj)
+    template_name = 'app1/student_form.html'
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list_url')
+    context = {'form':form}
+    return render(request, template_name, context)
+
+def delete_student(request, pk):
+    obj = Student.objects.get(pk=pk)
+    template_name = 'app1/delete_student.html'
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('student_list_url')
+    return render(request, template_name)
+
+
+
+
 
 
